f094bef908b8bd62c643d2baaffcf191e86b1baf
Rafael <rafaelsalass20@gmial.com>
2016-10-27 15:56:16 -0400

    simple linkedList done

--- main.cpp
+++ main.cpp
@@ -6,10 +6,20 @@ int main(int argc, char const *argv[]) {
   linkedList = new MyLinkedList<int>();
 
   linkedList->orderedPush(5);
+  linkedList->orderedPush(33);
   linkedList->orderedPush(25);
   linkedList->orderedPush(12);
   linkedList->orderedPush(3);
-  linkedList->orderedPush(33);
+  linkedList->orderedPush(11);
+  linkedList->orderedPush(34);
+  linkedList->orderedPush(76);
+  linkedList->orderedPush(34);
+  linkedList->orderedPush(23);
+  linkedList->orderedPush(54);
+  linkedList->orderedPush(120);
+  linkedList->orderedPush(30);
+  linkedList->orderedPush(142);
+  linkedList->orderedPush(39);
   linkedList->print();
   return 0;
 }
