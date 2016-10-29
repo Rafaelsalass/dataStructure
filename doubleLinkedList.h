e8349328d36a4940cfc1bebe7852a1672cbf7cfb
Rafael <rafaelsalass20@gmial.com>
2016-10-29 01:56:04 -0400

    start of doubleLinkedList

--- doubleLinkedList.h
+++ doubleLinkedList.h
@@ -7,10 +7,78 @@ template <class t>
 class doubleLinkedList{
 private:
   node<t>* head;
-  node<t>* p;
+  node<t>* tail;
 
 public:
-
+  doubleLinkedList();
+  void addHead(t element);
+  void addTail(t element);
+  void removeHead();
+  void removeTail();
+  void orderedPush(t element);
+  void fowardPrint();
+  void reversePrint();
+  bool isEmpty();
 };
+
+template <class t>
+doubleLinkedList<t>::doubleLinkedList(){
+  head = tail = NULL;
+}
+
+template <class t>
+bool doubleLinkedList<t>::isEmpty(){
+  return (head == NULL)? true : false;
+}
+
+template <class t>
+void doubleLinkedList<t>::addHead(t element){
+  node<t>* newNode = new node<t>(element);
+  if (!head) {
+    head = tail = newNode;
+    return;
+  }
+  head->setPrevious(newNode);
+  newNode->setNext(head);
+  head = newNode;
+}
+
+template <class t>
+void doubleLinkedList<t>::addTail(t element){
+  node<t>* newNode = new node<t>(element);
+  if (!tail) {
+    head = tail = newNode;
+    return;
+  }
+  tail->setNext(newNode);
+  newNode->setPrevious(tail);
+  tail = newNode;
+}
+
+template <class t>
+void doubleLinkedList<t>::fowardPrint(){
+  node<t>* p;
+  if(!head){
+    cout << "doule list is empty" << endl;
+    return;
+  }
+  cout << endl << endl;
+  for (p = head; p != NULL; p = p->getnext()) {
+    cout << p->getInfo() << endl;
+  }
+}
+
+template <class t>
+void doubleLinkedList<t>::reversePrint(){
+  node<t>* p;
+  if(!tail){
+    cout << "doule list is empty" << endl;
+    return;
+  }
+  cout << endl << endl;
+  for (p = tail; p != NULL; p = p->getPrevious()) {
+    cout << p->getInfo() << endl;
+  }
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include "MyLinkedList.h"
-
+#include "doubleLinkedList.h"
 
 int main(int argc, char const *argv[]) {
-  MyLinkedList<int> *linkedList;
+  /*MyLinkedList<int> *linkedList;
   linkedList = new MyLinkedList<int>();
   linkedList->removeTail();
   linkedList->orderedPush(5);
   linkedList->orderedPush(33);
   linkedList->orderedPush(25);
   linkedList->orderedPush(12);
-  linkedList->orderedPush(3);
-  linkedList->orderedPush(11);
-  linkedList->orderedPush(34);
-  linkedList->orderedPush(76);
-  linkedList->orderedPush(34);
-  linkedList->orderedPush(23);
-  linkedList->orderedPush(54);
-  linkedList->orderedPush(120);
-  linkedList->orderedPush(30);
-  linkedList->orderedPush(142);
   linkedList->orderedPush(39);
   linkedList->addHead(21);
   linkedList->addTail(90);
@@ -30,6 +20,20 @@ int main(int argc, char const *argv[]) {
     linkedList->print();
   }
   linkedList->removeTail();
-  linkedList->print();
+  linkedList->print();*/
+  doubleLinkedList<int> *dLinkedList;
+  dLinkedList = new doubleLinkedList<int>();
+
+  dLinkedList->addHead(20);
+  dLinkedList->addHead(23);
+  dLinkedList->addHead(27);
+  dLinkedList->addHead(21);
+  dLinkedList->fowardPrint();
+  dLinkedList->addTail(23);
+  dLinkedList->fowardPrint();
+  dLinkedList->addTail(12);
+  dLinkedList->fowardPrint();
+  dLinkedList->addTail(65);
+  dLinkedList->fowardPrint();
   return 0;
 }
