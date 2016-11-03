085ccde2a0573fc70fe201478467a44bbba738a4
Rafael <rafaelsalass20@gmial.com>
2016-11-02 23:20:22 -0400

    priorityQueue: missing removeElement method

--- priorityQueue.h
+++ priorityQueue.h
@@ -6,7 +6,7 @@
 #include "node.h"
 
 /* this class requires you to have and int paramater, to define
-priority (from 0, the smaller the value the more important),
+priority (the smaller the value the more important),
 and you also need to implement int getPriority()
 */
 
@@ -22,10 +22,12 @@ private:
 public:
   priorityQueue();
   bool firstOcurrence(int find);
+  void lastOcurrence(int find);
   bool isEmpty();
   void add(t element);
   void remove(t &element, bool band = true, int find = 0);
   bool setPosition();
+  void print();
 };
 
 template <class t>
@@ -71,5 +73,44 @@ bool priorityQueue<t>::firstOcurrence(int find){
   return false;
 }
 
+template <class t>
+void priorityQueue<t>::lastOcurrence(int find){
+  if (this->firstOcurrence(find)) {
+    while (position) {
+      if(position->getnext()->getInfo()->getPriority() >
+      position->getInfo()->getPriority() ||
+      position->getnext() == NULL){
+          return;
+      }else{
+        position = position->getnext();
+      }
+    }
+  }
+}
+
+template <class t>
+void priorityQueue<t>::add(t element){
+  node<t>* newNode = new node<t>(element);
+  node<t>* aux;
+  if (!head) {
+    head = tail = position = newNode;
+    return;
+  }else if (newNode->getInfo()->getPriority() <
+  head->getInfo()->getPriority()) {
+    newNode->setNext(head);
+    head = newNode;
+    return;
+  }else if (newNode->getInfo()->getPriority() >
+  tail->getInfo()->getPriority()) {
+    tail->setNext(newNode);
+    tail = newNode;
+    return;
+  }else{
+    this->lastOcurrence();
+    aux = position->getnext();
+    position->setNext(newNode);
+    newNode->setNext(aux);
+  }
+}
 
 #endif
